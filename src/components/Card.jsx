602d333bf3eff42d4c1782b45a4bae48b38a1ddb
Tetyana <tetyana.chouteau@gmail.com>
2023-09-19 17:47:01 +0200

    Affichage de chaque logement - Card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,26 +1,14 @@
 import React from 'react';
-import styles from "./Card.module.css"
+import styles from './Card.module.css';
 
-function Card() {
-  const title = "Card: Titre de logement";
-  const description = "Description de la carte";
-  const cover = "url_de_l_image.jpg";
-  const equipments = ["Équipement 1", "Équipement 2", "Équipement 3"];
-  const tags = ["Tag 1", "Tag 2", "Tag 3"];
-  const host = {
-    name: "Nom de l'hôte",
-    picture: "url_de_l_image_de_l_hote.jpg"
-  };
-  const rating = 4.5;
-  const location = "Emplacement de la carte";
+function Card({ data }) {
+  const { title, description, cover, equipments, tags, host, rating, location } = data;
 
   return (
-    
     <div className={styles.card}>
-      
       <h2>{title}</h2>
       <p>{description}</p>
-      <img src={cover} />
+      <img src={cover} alt={title} />
       <h3>Équipements:</h3>
       <ul>
         {equipments.map((equipment, index) => (
@@ -34,7 +22,7 @@ function Card() {
         ))}
       </ul>
       <div className="host">
-        <img src={host.picture} />
+        <img src={host.picture} alt={host.name} />
         <p>Hosted by {host.name}</p>
       </div>
       <p>Rating: {rating}</p>
--- src/pages/accueil.jsx
+++ src/pages/accueil.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import Card from '../components/Card';
 
+import data from '../data/logement.json';
+
 function Accueil() {
-    return (
-      <div>
-        <h1>Page - ACCUEIL</h1>
-        
-        <Card />
-        <p>Page - ACCUEIL fin </p>
-      </div>
-     
-    );
-  }
-  
+  return (
+    <div>
+      <h1>Page - ACCUEIL</h1>
+      {/* Passez les données aux composants Card */}
+      {data.map((logement, index) => (
+        <Card key={index} data={logement} />
+      ))}
+      <p>Page - ACCUEIL fin </p>
+    </div>
+  );
+}
 
 export default Accueil;
