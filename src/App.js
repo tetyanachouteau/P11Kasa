852e83815f3692cf145efa972ce06c71fbe8fe28
Tetyana <tetyana.chouteau@gmail.com>
2023-09-19 22:03:19 +0200

    Header, Navigation, css et mise à jour globale

--- src/App.js
+++ src/App.js
@@ -1,41 +1,16 @@
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route} from 'react-router-dom';
 import Accueil from "./pages/accueil";
 import Info from "./pages/info";
 import Location from "./pages/location";
 import Erreur from "./pages/erreur";
-import Footer from './components/Footer';
-import logo from "./images/logo192.png";
+
 import Layout from './components/Layout';
 
 function App() {
   return (
     <div className="App">
-      <header>
-        header debut
-        <img src={logo} alt="Logo de logement" className="logo" />
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Accueil</Link>
-            </li>
-            <li>
-              <Link to="/info">Info</Link>
-            </li>
-            <li>
-              <Link to="/location">Location</Link>
-            </li>
-            <li>
-              <Link to="/erreur">Erreur</Link>
-            </li>
-          </ul>
-        </nav>
-        header fin
-      </header>
-
       <Routes>
-        <Route
-          path="/"
-          element={<Layout />}
+        <Route path="/" element={<Layout />}
         >
           <Route path="/" element={<Accueil />} />
           <Route path="/info" element={<Info />} />
@@ -44,11 +19,6 @@ function App() {
         </Route>
       </Routes>
 
-      <main>
-        <article></article>
-      </main>
-      
-      <Footer />
     </div>
   );
 }
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,39 +1,19 @@
 import React from 'react';
-
-import { Outlet, Link } from 'react-router-dom';
+import Header from './Header';
+import Footer from './Footer';
+import { Outlet } from 'react-router-dom';
 
 function Layout() {
   return (
-    <div className="App">
-      <header>
-        header debut
-       <nav>
-          <ul>
-            <li>
-              <Link to="/">Accueil</Link>
-            </li>
-            <li>
-              <Link to="/info">Info</Link>
-            </li>
-            <li>
-              <Link to="/location">Location</Link>
-            </li>
-            <li>
-              <Link to="/erreur">Erreur</Link>
-            </li>
-          </ul>
-        </nav>
-        header fin
-      </header>
+    <div className="Layout">
+      <Header/>
 
       <main>
-        <Outlet /> 
+        <Outlet />
       </main>
 
-      <footer>
-        <p>footer debut</p>
-        footer fin
-      </footer>
+
+      <Footer />
     </div>
   );
 }
