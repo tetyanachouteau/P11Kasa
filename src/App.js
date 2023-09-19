e63f51a72b79ba4ff311f74bf6aa07340dedad5e
Tetyana <tetyana.chouteau@gmail.com>
2023-09-19 22:58:35 +0200

    //tâches pour: page 404, route, info Collapse, figma

--- src/App.js
+++ src/App.js
@@ -6,6 +6,18 @@ import Erreur from "./pages/erreur";
 
 import Layout from './components/Layout';
 
+//La page 404 est renvoyée pour chaque route inexistante, ou si une
+//valeur présente dans l’URL ne fait pas partie des données renseignées.
+//Créer un composant à part pour le routeur.
+//Collapse doit avoir deux états différents : il peut être ouvert ou fermé.
+//● Pensez à bien utiliser les propriétés des composants pour passer les
+//données voulues.
+//● Souvenez-vous que votre composant peut prendre différentes formes en
+//fonction de son utilisation.
+//Suivez bien le design de la maquette.
+//● Assurez-vous que toute route inexistante mène bien à la page d’erreur
+//https://reactrouter.com/en/dev/route/error-element
+
 function App() {
   return (
     <div className="App">
