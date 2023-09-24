88e1161d40d005692deea60252b5a8406d876eb4
Tetyana <tetyana.chouteau@gmail.com>
2023-09-24 18:41:07 +0200

    Card dynamique

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,11 +3,11 @@ import styles from './Card.module.css';
 import { NavLink } from 'react-router-dom';
 
 function Card({ data }) {
-  const { title, cover } = data;
+  const { title, cover, id } = data;
 
   return (
     <div className={styles.card}>
-      <NavLink exact to="/location/1" activeClassName={styles.activeLink}>
+      <NavLink exact to={ "/location/" + id} activeClassName={styles.activeLink}>
       <h2>{title}</h2>
     
       <img src={cover} alt={title} />
--- src/pages/location.jsx
+++ src/pages/location.jsx
@@ -1,10 +1,16 @@
+import { useParams } from "react-router-dom";
 import Slideshow from "../components/Slideshow";
+import data from '../data/logement.json';
 
 function Location() {
+    let { id } = useParams()
+
+    let logement = data.find(el => el.id === id )
+
     return (
       <div >
-        <h1>Page - LOCATION</h1>
-        <Slideshow images={['/images/Background1.png', '/images/Background2.png', '/images/Background3.png']}/>
+        <h1>{logement.title}</h1>
+        <Slideshow images={logement.pictures}/>
         <p >Page - Location fin</p>
       </div>
     );
