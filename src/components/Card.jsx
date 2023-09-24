23892428134911a658dc600cb7f53b5af731ea01
Tetyana <tetyana.chouteau@gmail.com>
2023-09-24 17:08:03 +0200

    Ajout du lien sur la carte

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import styles from './Card.module.css';
+import { NavLink } from 'react-router-dom';
 
 function Card({ data }) {
   const { title, cover } = data;
 
   return (
     <div className={styles.card}>
+      <NavLink exact to="/location/1" activeClassName={styles.activeLink}>
       <h2>{title}</h2>
     
       <img src={cover} alt={title} />
-      
+      </NavLink>
     </div>
   );
 }
