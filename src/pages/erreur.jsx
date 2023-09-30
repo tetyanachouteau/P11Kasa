1804072383225428fdb3e6410503ef563a139fc8
Tetyana <tetyana.chouteau@gmail.com>
2023-09-30 20:14:10 +0200

    Mise à jour erreur.jsx et css

--- src/pages/erreur.jsx
+++ src/pages/erreur.jsx
@@ -1,12 +1,15 @@
 import styles from "./erreur.module.css"
-//import styles from "./Erreur.module.css"
+import { NavLink } from 'react-router-dom';
 
 function Erreur() {
     return (
       <div className={styles.erreur}>
-       <p>Page - ERREUR</p> 
-Hello!
-        <p>Page - ERREUR fin</p>
+       <img src="../images/404.png" alt="Erreur 404" />
+        <p>Oups! La page que vous demandez n'existe pas.</p>
+        <NavLink exact to="/" className={({isActive}) => (isActive ? styles.activeLink : "")}>
+        Retourner sur la page d’accueil
+          </NavLink>
+        
       </div>
     );
   }
