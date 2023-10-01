9ed3af27bbbea54d9cf11b45d6893eee965dd46f
Tetyana <tetyana.chouteau@gmail.com>
2023-10-01 15:42:06 +0200

    Tous les collopases page info

--- src/pages/info.jsx
+++ src/pages/info.jsx
@@ -5,17 +5,18 @@ function Info() {
     return (
       <div>
         <Banner image="../images/fond2.png"/>
-        <h1>Page - INFO</h1>
-        <Collapse entete="test de titre">
-          <p >Climatisation
-Wi-Fi
-Cuisine
-Espace de travail
-Fer à repasser
-Sèche-cheveux
-Cintres</p>
+        <Collapse entete="FIabilité">
+          <p >Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes.</p>
         </Collapse>
-      
+        <Collapse entete="Respect">
+          <p >La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.</p>
+        </Collapse>
+        <Collapse entete="Service">
+          <p >La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.</p>
+        </Collapse>
+        <Collapse entete="Sécurité">
+          <p >La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.</p>
+        </Collapse>
       </div>
     );
   }
