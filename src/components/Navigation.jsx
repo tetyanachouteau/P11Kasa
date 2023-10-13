53940a879526f5293e156f61e87e5950f789e572
Tetyana <tetyana.chouteau@gmail.com>
2023-10-13 16:30:39 +0200

    Ajout // à la navigation

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,6 +1,26 @@
 import styles from "./Navigation.module.css"
 import { NavLink } from 'react-router-dom';
 
+//NavLink: composant fourni par React Router pour créer des liens
+// de navigation dans une application React. 
+
+//to="/": C'est la prop to de NavLink. Elle spécifie la destination vers
+// laquelle le lien doit rediriger. 
+//Lien redirige vers la page d'accueil, qui est généralement définie
+// avec le chemin "/" dans React Router.
+
+//className={({ isActive }) => (isActive ? styles.activeLink : ""): 
+//C'est la prop className de NavLink. Cette prop permet de définir la classe CSS 
+//qui sera appliquée au lien. Fonction fléchée qui prend un argument isActive. 
+//Cette fonction vérifie si le lien est actif (c'est-à-dire si l'URL 
+//correspond au chemin spécifié dans to) et, si c'est le cas, 
+//elle applique la classe CSS styles.activeLink. 
+//Sinon, elle applique une chaîne de classe vide (""), 
+//ce qui signifie qu'aucune classe supplémentaire n'est appliquée.
+
+//Accueil: C'est le texte ou le contenu du lien qui sera affiché à l'utilisateur. 
+
+// Définition du composant "Navigation"          
 function Navigation() {
   return (
     <div className={styles.navigation}>
